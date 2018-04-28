ae62cdf904c22eed95e3b5a8b132454e240e9843
Tiger Sachse <tiger@sachse.io>
2018-04-28 14:07:55 -0400

    make install dots work from anywhere

--- dots/install_dots.sh
+++ dots/install_dots.sh
@@ -1,7 +1,9 @@
-cp .bashrc ~/
-cp .bash_aliases ~/
-cp .bash_profile ~/
-cp .face ~/
-cp .Xresources ~/
-cp -r .config/nvim ~/.config
-cp -r .config/shellcuts ~/.config
+D_DOTS="$HOME/.scripts/dots"
+
+cp $D_DOTS/.bashrc ~/
+cp $D_DOTS/.bash_aliases ~/
+cp $D_DOTS/.bash_profile ~/
+cp $D_DOTS/.face ~/
+cp $D_DOTS/.Xresources ~/
+cp -r $D_DOTS/.config/nvim ~/.config
+cp -r $D_DOTS/.config/shellcuts ~/.config
