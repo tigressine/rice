d6d013bcf9b3f41b135e8a78cc9af1e0f7d97a8e
Tiger Sachse <tiger@sachse.io>
2018-04-29 18:20:19 -0400

    reworked to better segment script

--- scripts/ubuntu_desktop_setup.sh
+++ scripts/ubuntu_desktop_setup.sh
@@ -1,101 +1,183 @@
 # Full desktop installation script for Ubuntu systems
 # Written by Tiger Sachse
 
-### FUNCTIONS ###
-function procedure_check {
-    echo
-    echo "Next process: $1"
-    echo "Proceed? (Yes/No/Skip)"
-    
-    read command
-    
-    case $command in
-        Y|y|yes|Yes)
-            echo "Proceeding..."
-            return 0
-            ;;
-        S|s|skip|Skip)
-            echo "Skipping..."
-            return 1
-            ;;
-        *)
-            echo "Goodbye!"
-            exit 0
-            ;;
-    esac
-}
-
-
 ### SCRIPT DIRECTORY ###
 D_SCRIPTS="$HOME/.scripts"
 
+### GENERAL FUNCTIONS ###
+# Entry point of the script
+function main {
+    echo "Welcome to Tgsachse's installation script for Ubuntu."
+    echo
+    echo "Below is a list of functions this script provides. Enter the letter"
+    echo "or letters next to the functions you would like to run. You do not need"
+    echo "to separate the letters."
+    echo
+    echo "a) run all the functions"
+    echo "b) install git"
+    echo "c) initialize script"
+    echo "d) update system"
+    echo "e) install utilities and fun stuff"
+    echo "f) install SpOnGe"
+    echo "g) enable UFW firewall"
+    echo "h) clean up home directory"
+    echo "i) install dots"
+    echo "j) install icon, GTK, and shell themes"
+    echo "k) install Java 10"
+    echo "l) install pip for Python3, and Python projects"
+    echo "m) install Google Chrome"
+    echo "n) install and enable Dropbox"
+    echo "o) install Discord"
+    echo "p) install Minecraft"
+    echo "q) quit"
+    echo
+    echo "e.g. To install Discord, Google Chrome, and Java 10, enter \"kmo\""
+    echo
+
+    read COMMANDS
+    LEN=${#COMMANDS}
+
+    for ((i = 0; i < LEN; i++)); do
+        case ${COMMANDS:$i:1} in
+            a|A)
+                run_all
+                break
+                ;;
+            b|B)
+                install_git
+                ;;
+            c|C)
+                download_scripts
+                ;;
+            d|D)
+                update_system
+                ;;
+            e|E)
+                install_utilities
+                ;;
+            f|F)
+                install_sponge
+                ;;
+            g|G)
+                enable_firewall
+                ;;
+            h|H)
+                configure_home_directory
+                ;;
+            i|I)
+                install_dots
+                ;;
+            j|J)
+                install_themes
+                ;;
+            k|K)
+                install_java
+                ;;
+            l|L)
+                install_python_tools
+                ;;
+            m|M)
+                install_chrome
+                ;;
+            n|N)
+                install_dropbox
+                ;;
+            o|O)
+                install_discord
+                ;;
+            p|P)
+                install_minecraft
+                ;;
+            q|Q)
+                exit
+                ;;
+        esac
+    done
+}
+
+# run all other functions
+function run_all {
+    install_git
+    download_scripts
+    update_system
+    install_utilities
+    install_sponge
+    enable_firewall
+    configure_home_directory
+    install_dots
+    install_themes
+    install_java
+    install_python_tools
+    install_chrome
+    install_dropbox
+    install_discord
+    install_minecraft
+}
 
 ### SETUP ###
 # install git to clone the scripts repo
-if procedure_check "install git";
-then
+function install_git {
     sudo apt install git -y
-    git config --global user.email "tgsachse@gmail.com"
-    git config --global user.name "Tiger Sachse"
-fi
+
+    echo "Enter email for git:"
+    read EMAIL
+    echo "Enter full name for git:"
+    read NAME
+
+    git config --global user.email $EMAIL
+    git config --global user.name $NAME
+}
 
 # Clone script repository
-if procedure_check "prepare for installation";
-then
+function download_scripts {
     if [ -d $D_SCRIPTS ];
     then
         sudo rm -r $D_SCRIPTS
     fi
     git clone https://www.github.com/tgsachse/scripts.git $D_SCRIPTS
-fi
+}
 
 # Check that system is up to date
-if procedure_check "update system";
-then
+function update_system {
     sudo apt update
     sudo apt upgrade -y
-fi
+}
 
 
 ### UTILITIES ###
 # Install useful terminal utilities, UCF VPN, and goofy stuff
-if procedure_check "install utilities and goofy stuff";
-then
+function install_utilities {
     sudo apt install -y neovim vpnc network-manager-vpnc tree zsh fish gcc
-    sudo apt install -y fortune cowsay lolcat
-fi
+    sudo apt install -y fortune cowsay lolcat screenfetch
+}
 
 # Run SpOnGe install script
-if procedure_check "run SpOnGe script";
-then
+function install_sponge {
     bash $D_SCRIPTS/scripts/install_sponge.sh
-fi
+}
 
-if procedure_check "enable firewall";
-then
+# Enable UFW firewall
+function enable_firewall {
     sudo ufw enable
-fi
+}
 
 
 ### CUSTOMIZATION ###
-#
-if procedure_check "configure home folder";
-then
+# Configure home directory
+function configure_home_directory {
     rm -r $HOME/Music $HOME/Pictures $HOME/Public
     rm -r $HOME/Downloads $HOME/Templates $HOME/Videos
     rm -r $HOME/Desktop $HOME/Documents
     mkdir $HOME/Bin $HOME/Slate
-fi
+}
 
 # Install dotfiles from GitHub
-if procedure_check "install dotfiles";
-then
+function install_dots {
     bash $D_SCRIPTS/dots/install_dots.sh
-fi
+}
 
 # Install icon theme, GTK theme and enable shell themes
-if procedure_check "install themes";
-then
+function install_themes {
     sudo add-apt-repository ppa:tista/adapta
     sudo add-apt-repository ppa:papirus/papirus
     sudo apt update
@@ -107,56 +189,68 @@ then
     echo
     echo "You'll need to restart and then use the tweak tool to change the shell theme."
     echo
-fi
+}
+
+# Install wallpaper manager
+function install_variety { # ADD TO SWITCH
+    sudo add-apt-repository ppa:peterlevi/ppa
+    sudo apt update
+    sudo apt install variety
+}
 
 
 ### DEVELOPMENT TOOLS ###
 # Install Java10
-if procedure_check "install Java";
-then
+function install_java {
     sudo add-apt-repository ppa:linuxuprising/java
     sudo apt update
     sudo apt install -y oracle-java10-installer
-fi
+}
 
 # Install Python tools and projects
-if procedure_check "install pip and python stuff";
-then
+function install_python_tools {
     sudo apt install -y python3-pip
     /usr/bin/yes | sudo pip3 install selenium enigmamachine
-fi
+}
 
 
 ### SOFTWARE ###
 # Install Google Chrome
-if procedure_check "install Chrome";
-then
+function install_chrome {
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
     echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
     sudo apt update
     sudo apt install -y google-chrome-stable
-fi
+}
 
 # Install Dropbox
-if procedure_check "install and autostart Dropbox";
-then
+function install_dropbox {
     sudo apt install -y nautilus-dropbox
     dropbox autostart y
-fi
+}
 
 
 ### GAMING ###
 # Install Discord
-if procedure_check "install Discord";
-then
+function install_discord {
     wget -O /tmp/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
     sudo apt install libgconf-2-4 libc++1
     sudo apt --fix-broken install
     sudo dpkg -i /tmp/discord.deb
-fi
+}
 
 # Install Minecraft
-if procedure_check "install Minecraft";
-then
+function install_minecraft {
     bash $D_INSTALL/scripts/install_minecraft.sh
-fi
+}
+
+
+# call the main function
+#main
+
+function check_installed {
+    return command -v $1
+}
+
+echo $(check_installed "git")
+echo $(check_installed "gitt")
