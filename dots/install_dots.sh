562e5c3d5ebe06bb5191f1b20fdca0f16b26a95d
Tiger Sachse <tiger@sachse.io>
2018-05-05 14:49:58 -0400

    Remove Xresources

--- dots/install_dots.sh
+++ dots/install_dots.sh
@@ -4,7 +4,6 @@ cp $D_DOTS/.bashrc ~/
 cp $D_DOTS/.bash_aliases ~/
 cp $D_DOTS/.bash_profile ~/
 cp $D_DOTS/.face ~/
-cp $D_DOTS/.Xresources ~/
 cp -r $D_DOTS/.config/nvim ~/.config
 cp -r $D_DOTS/.config/shellcuts ~/.config
 cp -r $D_DOTS/.config/variety ~/.config
--- dots/update_dots.sh
+++ dots/update_dots.sh
@@ -2,7 +2,6 @@ cp $HOME/.bashrc .
 cp $HOME/.bash_aliases .
 cp $HOME/.bash_profile .
 cp $HOME/.face .
-cp $HOME/.Xresources .
 cp -r $HOME/.config/nvim .config/
 cp -r $HOME/.config/shellcuts .config/
 cp -r $HOME/.config/variety .config/
