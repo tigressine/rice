5ed29f30302ef4b1a8f8cd03a2f20da3256c7bd5
Tiger Sachse <tiger@sachse.io>
2020-10-27 23:00:04 -0700

    Reduce spacing to 2 spaces.

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 # Install dots located in this repository.
-# Written by Tiger Sachse.
 
 mkdir -p "$HOME/.config/nvim/"
 mkdir -p "$HOME/.config/shell/"
@@ -16,24 +15,24 @@ cp dots/.config/nvim/init.vim "$HOME/.config/nvim/"
 cp dots/.config/shell/shellrc "$HOME/.config/shell/"
 cp dots/.config/shell/aliases/core_aliases "$HOME/.config/shell/aliases/"
 cp dots/.config/shell/configuration/core_configuration \
-    "$HOME/.config/shell/configuration"
+  "$HOME/.config/shell/configuration/"
 
 # Copy example local-override files onto the machine if no local overrides
 # exist.
 if [ ! -f "$HOME/.config/shell/configuration/local_configuration" ]; then
-    cp dots/.config/shell/configuration/local_configuration \
-        "$HOME/.config/shell/configuration"
+  cp dots/.config/shell/configuration/local_configuration \
+    "$HOME/.config/shell/configuration/"
 fi
 if [ ! -f "$HOME/.config/shell/aliases/local_aliases" ]; then
-    cp dots/.config/shell/aliases/local_aliases "$HOME/.config/shell/aliases"
+  cp dots/.config/shell/aliases/local_aliases "$HOME/.config/shell/aliases/"
 fi
 
 # If a core installation is not requested, install extended configuration
 # and alias files.
 if [ "$1" != --core ]; then
-    cp dots/.config/shell/aliases/extended_aliases \
-        "$HOME/.config/shell/aliases/"
-    cp dots/.config/shell/configuration/extended_configuration \
-        "$HOME/.config/shell/configuration"
-    cat dots/.dconf | dconf load /
+  cp dots/.config/shell/aliases/extended_aliases \
+    "$HOME/.config/shell/aliases/"
+  cp dots/.config/shell/configuration/extended_configuration \
+    "$HOME/.config/shell/configuration/"
+  cat dots/.dconf | dconf load /
 fi
--- sync.sh
+++ sync.sh
@@ -1,25 +1,24 @@
 # Update the dots located in this repository.
-# Written by Tiger Sachse.
 
 DCONF_SETTINGS_PATHS="
-    org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0
-    org/gnome/settings-daemon/plugins/media-keys
-    org/gnome/shell/keybindings
-    org/gnome/shell/extensions/dash-to-dock
-    org/gnome/desktop/wm/keybindings
-    org/gnome/mutter/keybindings
+  org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0
+  org/gnome/settings-daemon/plugins/media-keys
+  org/gnome/shell/keybindings
+  org/gnome/shell/extensions/dash-to-dock
+  org/gnome/desktop/wm/keybindings
+  org/gnome/mutter/keybindings
 "
 
 # Save specific dconf settings into a file.
 rm -f dots/.dconf
 for path in $DCONF_SETTINGS_PATHS; do
-    echo "[$path]" >> dots/.dconf
-    dconf dump "/$path/" \
-        | sed -n '1!p' \
-        | awk '/^$/{exit} {print $0}' \
-        | sort \
-        >> dots/.dconf
-    echo "" >> dots/.dconf
+  echo "[$path]" >> dots/.dconf
+  dconf dump "/$path/" \
+    | sed -n '1!p' \
+    | awk '/^$/{exit} {print $0}' \
+    | sort \
+    >> dots/.dconf
+  echo "" >> dots/.dconf
 done
 
 cp "$HOME/.face" dots/
@@ -39,6 +38,6 @@ cp "$HOME/.config/shell/shellrc" dots/.config/shell/
 cp "$HOME/.config/shell/aliases/core_aliases" dots/.config/shell/aliases/
 cp "$HOME/.config/shell/aliases/extended_aliases" dots/.config/shell/aliases/
 cp "$HOME/.config/shell/configuration/core_configuration" \
-    dots/.config/shell/configuration/
+  dots/.config/shell/configuration/
 cp "$HOME/.config/shell/configuration/extended_configuration" \
-    dots/.config/shell/configuration/
+  dots/.config/shell/configuration/
